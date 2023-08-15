74d8e5a97ab062d9142168c9b05811f9403095a1
Josaiah Fayne <120128690+jFayneF22@users.noreply.github.com>
2023-08-15 15:58:50 +0000

    commit

---
+++ 08_07_23/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    //Properties (Fields)
+    public String name = "Button";
+    public int    age = 4;
+    public String Breed = "ugly";
+    public String FavFood = "Fish";
+
+    //Behaviors(Methods)
+    public void displayCat () {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Breed: " + Breed);
+        System.out.println("Favorite Food: " + FavFood);
+    }
+}
